6145f762b8a4da294cd0c652721c2df1267291ac
Jürg Lehni <juerg@scratchdisk.com>
2007-12-08 15:39:18 +0000

    Updated json macro to support single quotes properly.

--- base/helma/Global/macros.js
+++ base/helma/Global/macros.js
@@ -1,17 +1,33 @@
 function json_macro(param, object) {
-	var filter = param.filter && param.filter.split(',').each(function(val) {
-		this[val] = true;
-	}, {});
-	var str;
-	if (filter && Base.type(object) == 'object') {
-		str = '{' + object.each(function(val, key) {
-			if (filter[key] && val != undefined)
-				this.push(key + ':' + Json.encode(val));
-		}, []).join(',') + '}';
-	} else {
-		str = Json.encode(object);
+	// If param.include is defined, only include the keys defined in that
+	// coma seperated string:
+	if (param.include) {
+		var include = param.include.split(',').each(function(val) {
+			this[val] = true;
+		}, {});
+		object = object.each(function(val, key) {
+			if (include[key])
+				this[key] = val;
+		}, {});
 	}
-	return str.replaceAll("'", "\\'").replaceAll('"', "'");
+	if (param.singleQuotes == 'true') {
+		// This is a bit of a hack:  Json.encode uses uneval internally, and this
+		// produces code like { key: "value", 'class': "content" }, using single quote
+		// for reserved keywords. This means we cannot simply replace like bellow
+		// if an object is passed, since the single quote then gets replaced by \'
+		// The workaround is to allways use .each even when the include filter is
+		// not defined
+		if (Base.type(object) == 'object') {
+		 	return '{' + object.each(function(val, key) {
+				if (val != undefined)
+					this.push("'" + key + "':" + Json.encode(val).replaceAll("'", "\\'").replaceAll('"', "'"));
+			}, []).join(',') + '}';
+		} else {
+			return Json.encode(object).replaceAll("'", "\\'").replaceAll('"', "'");
+		}
+	} else {
+		return Json.encode(object);
+	}
 }
 
 function link_macro(param) {	
