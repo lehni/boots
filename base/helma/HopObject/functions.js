93484755ddca7393254c879cc1564335b32b011c
Jürg Lehni <juerg@scratchdisk.com>
2008-01-21 22:00:03 +0000

    Added pagination from Scriptographer.

--- base/helma/HopObject/functions.js
+++ base/helma/HopObject/functions.js
@@ -32,6 +32,61 @@ HopObject.inject({
 		this.renderTemplate('html', param, res);
 	},
 
+	/**
+	 * Renders the navigation for a paginated ilst. param.position is read to determine the current position
+	 * and set if it's undefined.
+	 * param.maxPerPage specifies the amount of items per page
+	 * ...
+	 */
+	renderPagination: function(param, out) {
+		var last = Math.max(0, Math.floor((param.count - 1) / param.maxPerPage));
+		var pos = 0;
+		if (req.data.pos != null) {
+			pos = parseInt(req.data.pos);
+		} else if (param.position) {
+			pos = param.position;
+		} 
+		if (pos < 0) pos = last;
+		param.position = pos;
+		var index = pos * param.maxPerPage;
+		// Render pagination only if needed:
+		var multiPage = param.count > param.maxPerPage;
+		if (multiPage || param.prefix) {
+			if (multiPage) {
+				var href = param.href ? param.href : param.container ? this.href('posts') : this.href();
+				var htmlOptions = param.container ? { update: param.container } : null;
+				var pages = pos > 0 ? [ renderLink('&lt;', { href: href, query: 'pos=' + (pos - 1) }, htmlOptions) ] : [];
+				var step = 1;
+				var num = last;
+				while (num >= (step == 1 ? 15 : 7)) {
+					step++;
+					num = last / step; 
+				}
+				for (var i = 0; i <= last; i += step) {
+					var name = i + 1;
+					if (step > 1)
+						name += '-' + (name + step - 1);
+					if (pos >= i && pos < i + step)
+						pages.push(name);
+					else
+						pages.push(renderLink(name, { href: href, query: 'pos=' + i }, htmlOptions));
+				}
+				if (pos < last)
+					pages.push(renderLink('&gt;', { href: href, query: 'pos=' + (pos + 1) }, htmlOptions));
+				else
+					pages.push('<span>&gt;</span>');
+				param.pages = pages.join('&nbsp;');
+			}
+			if (param.count > 0) {
+				param.first = index + 1;
+				param.last = Math.min(param.count, index + param.maxPerPage);
+			}
+			if (param.plural == null)
+				param.plural = param.singular + 's';
+			return this.renderTemplate('pagination', param, out);
+		}
+	},
+
 	/** 
 	 * Returns the object's fulLId. This is the object's prototype and id
 	 * seperated by a dash. This id can be used again to retrieve the object
