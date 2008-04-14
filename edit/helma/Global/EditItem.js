00cc4cb906d519bce30b30f1286e643637551b68
Jürg Lehni <juerg@scratchdisk.com>
2008-04-14 14:41:26 +0000

    Added support for startYear to date.

--- edit/helma/Global/EditItem.js
+++ edit/helma/Global/EditItem.js
@@ -256,8 +256,7 @@ DateItem = EditItem.extend({
 			for (var i = start; i <= end; i++) {
 				options.push({ 
 					value: i,
-					name: months ? new Date(2000, i, 1).format(format) :
-						i.format(format),
+					name: months ? new Date(2000, i, 1).format(format) : i.format(format),
 					selected: i == value
 				});
 			}
@@ -270,24 +269,18 @@ DateItem = EditItem.extend({
 		var now = new Date();
 		var date = value ? value : now;
 		if (this.day)
-			renderSelect(name + '_day', 1, 31, '00',
-				date.getDate());
+			renderSelect(name + '_day', 1, 31, '00', date.getDate());
 		if (this.month)
-			renderSelect(name + '_month', 0, 12, 'MMMM',
-				date.getMonth());
+			renderSelect(name + '_month', 0, 12, 'MMMM', date.getMonth());
 		if (this.year)
-			// TODO: app properties for start and end point?
-			renderSelect(name + '_year', 1999, now.getFullYear() + 2, '0000',
-				date.getFullYear());
+			renderSelect(name + '_year', this.startYear || 2000,
+				now.getFullYear() + 2, '0000', date.getFullYear());
 		if (this.hours)
-			renderSelect(name + '_hours', 0, 23, '00',
-				date.getHours());
+			renderSelect(name + '_hours', 0, 23, '00', date.getHours());
 		if (this.minutes)
-			renderSelect(name + '_minutes', 0, 59, '00',
-				date.getMinutes());
+			renderSelect(name + '_minutes', 0, 59, '00', date.getMinutes());
 		if (this.seconds)
-			renderSelect(name + '_seconds', 0, 59, '00',
-				date.getSeconds());
+			renderSelect(name + '_seconds', 0, 59, '00', date.getSeconds());
 	},
 
 	convert: function(value) {
