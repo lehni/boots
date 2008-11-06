ad036bae45fa023bd56f7821f50fa771e040f37d
Jürg Lehni <juerg@scratchdisk.com>
2008-11-06 21:22:56 +0000

    Reformatted Object wrapping to one static ObjectWrapper.wrap function, which takes care of object and xml objects. More type could be added in the future.

--- base/helma/Global/ObjectWrapper.js
+++ base/helma/Global/ObjectWrapper.js
@@ -1,124 +1,122 @@
-// Produces a wrapper that inherits from object and defines a __meta__ object
-// to intercept modifications and again wrap returned values, in order to
-// detect modifications on any level down the hierarchy.
-
-ObjectWrapper = Base.extend(new function() {
+ObjectWrapper = new function() {
+	var Scriptable = Packages.org.mozilla.javascript.Scriptable;
 	var ScriptableObject = Packages.org.mozilla.javascript.ScriptableObject;
-	var Scriptable = Packages.org.mozilla.javascript.Scriptable;
-
-	return {
-		initialize: function(obj, onChange) {
-			obj = toJava(obj);
-
-			var ret = new JavaAdapter(ScriptableObject, {
-				get: function(name, start) {
-					var value = obj.get(name, obj);
-					// Wrap sub elements in ObjectWrappers again
-					return value != Scriptable.NOT_FOUND && typeof value == 'object'
-						? new ObjectWrapper(value, onChange)
-						: value;
-				},
-
-				put: function(name, start, value) {
-					obj.put(name, obj, value);
-					if (onChange)
-						onChange.call(this);
-				},
-
-				has: function(name, start) {
-					return obj.has(name, obj);
-				},
-
-				'delete': function(name) {
-					obj['delete'](name);
-					if (onChange)
-						onChange.call(this);
-				},
-
-				getDefaultValue: function(hint) {
-					return obj.getDefaultValue(hint);
-				},
-
-				toString: function() {
-					return obj.toString();
-				}
-			});
-			toJava(ret).setPrototype(obj);
-			return ret;
-		}
-	}
-});
-
-
-// Making an object wrapper for XMLObjects is a bit more tricky. Due to the structure
-// of things in Rhino, we need to wrap them directly in a class inheriting from
-// org.mozilla.javascript.xml.XMLObject, and override the various ecma commands.
-// Other than that, this is identical in structure to XMLObjectWrapper
-XMLObjectWrapper = ObjectWrapper.extend(new function() {
 	var XMLObject = Packages.org.mozilla.javascript.xml.XMLObject;
-	var Scriptable = Packages.org.mozilla.javascript.Scriptable;
 
 	return {
-		initialize: function(xml, onChange) {
-			xml = toJava(xml);
+		// Produces a wrapper that inherits from object, intercepts modifications
+		// and calls a onChange handler if they happen. It also wraps returned
+		// values again, in order to detect modifications on any level down the
+		// hierarchy.
+		wrap: function(obj, onChange) {
+			if (obj == Scriptable.NOT_FOUND)
+				return obj;
+			var type = typeof obj;
+			if (type == 'object' || type == 'xml') {
+				// Retrieve the NativeJavaObject wrapper for this object, so we can
+				// access its java methods directly from the wrapper that we are going
+				// to produce bellow.
+				obj = toJava(obj);
+				var adapter;
+				if (type == 'object') {
+					// Wrap native objects in a ScriptableObject that delegates the function calls
+					// and watches for things to change.
+					adapter = new JavaAdapter(ScriptableObject, {
+						get: function(name, start) {
+							// Wrap sub elements again
+							return ObjectWrapper.wrap(obj.get(name, obj), onChange);
+						},
 
-			var ret = new JavaAdapter(XMLObject, {
-				ecmaGet: function(cx, id) {
-					var value = xml.ecmaGet(cx, id);
-					// Wrap sub elements in XMLObjectWrappers again
-					return value != Scriptable.NOT_FOUND && typeof value == 'xml'
-						? new XMLObjectWrapper(value, onChange)
-						: value;
-				},
+						put: function(name, start, value) {
+							obj.put(name, obj, value);
+							if (onChange)
+								onChange.call(this);
+						},
 
-				ecmaPut: function(cx, id, value) {
-					xml.ecmaPut(cx, id, value);
-					if (onChange)
-						onChange.call(this);
-				},
+						has: function(name, start) {
+							return obj.has(name, obj);
+						},
 
-				ecmaHas: function(cx, id) {
-					return xml.ecmaHas(cx, id);
-				},
+						'delete': function(name) {
+							obj['delete'](name);
+							if (onChange)
+								onChange.call(this);
+						},
 
-				ecmaDelete: function(cx, id) {
-					var ret = xml.ecmaDelete(cx, id);
-					if (ret && onChange)
-						onChange.call(this);
-					return ret;
-				},
+						getDefaultValue: function(hint) {
+							return obj.getDefaultValue(hint);
+						},
 
-				enterDotQuery: function(scope) {
-					return xml.enterDotQuery(scope);
-				},
+						toString: function() {
+							return obj.toString();
+						}
+					});
+				} else {
+					// Make a special wrapper for XMLObjects. Due to the structure of things in Rhino,
+					// We need to wrap them directly in a class inheriting from XMLObject, and override
+					// the various ecma commands. Otherwise, E4X will not work with the wrapped object.
+					// Other than that, this is identical in structure to the object code above.
+					adapter = new JavaAdapter(XMLObject, {
+						ecmaGet: function(cx, id) {
+							// Wrap sub elements again
+							return ObjectWrapper.wrap(xml.ecmaGet(cx, id), onChange);
+						},
 
-				enterWith: function(scope) {
-					return xml.enterWith(scope);
-				},
+						ecmaPut: function(cx, id, value) {
+							xml.ecmaPut(cx, id, value);
+							if (onChange)
+								onChange.call(this);
+						},
 
-				getExtraMethodSource: function(cx) {
-					return xml.getExtraMethodSource(cx);
-				},
+						ecmaHas: function(cx, id) {
+							return xml.ecmaHas(cx, id);
+						},
 
-				memberRef: function() {
-					// varargs
-					return xml.memberRef.apply(xml, arguments);
-				},
+						ecmaDelete: function(cx, id) {
+							var ret = xml.ecmaDelete(cx, id);
+							if (ret && onChange)
+								onChange.call(this);
+							return ret;
+						},
 
-				getClassName: function() {
-					return xml.getClassName();
-				},
+						getDefaultValue: function(hint) {
+							return xml.getDefaultValue(hint);
+						},
 
-				getDefaultValue: function(hint) {
-					return xml.getDefaultValue(hint);
-				},
+						toString: function() {
+							return xml.toString();
+						},
 
-				toString: function() {
-					return xml.toString();
+						enterDotQuery: function(scope) {
+							return xml.enterDotQuery(scope);
+						},
+
+						enterWith: function(scope) {
+							return xml.enterWith(scope);
+						},
+
+						getExtraMethodSource: function(cx) {
+							return xml.getExtraMethodSource(cx);
+						},
+
+						memberRef: function() {
+							// varargs
+							return xml.memberRef.apply(xml, arguments);
+						},
+
+						getClassName: function() {
+							return xml.getClassName();
+						}
+					});
 				}
-			});
-			toJava(ret).setPrototype(xml);
-			return ret;
+				// Now set the adapter's prototype ot the object, so it inherits
+				// all fields form it.
+				toJava(adapter).setPrototype(obj);
+				return adapter;
+			} else {
+				// Basic types, no wrapping needed to detect change
+				return obj;
+			}
 		}
 	}
-});
+}
