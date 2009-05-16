4af293f2b10990b2d5a28dca052a7748180246b4
Jürg Lehni <juerg@scratchdisk.com>
2009-05-16 18:02:52 +0000

    Fixed notification bugs and added new app.properties setting called notificationTitle.

--- post/helma/Root/functions.js
+++ post/helma/Root/functions.js
@@ -7,14 +7,14 @@ Root.inject({
 			if (notifications.length > 0) {
 				var email = notifications[0].email;
 				var username = notifications[0].username;
-				app.log(username + " " + email);
+				app.log(username + ' ' + email);
 
 				res.push();
 				var numTopics = 0;
 				for (var j = 0; j < notifications.length; j++) {
 					var notification = notifications[j];
 					if (notification.node) {
-						this.renderTemplate("emailTopic", {
+						this.renderTemplate('emailTopic', {
 							notification: notification, 
 						}, res);
 						numTopics++;
@@ -23,19 +23,19 @@ Root.inject({
 				}
 				var nodes = res.pop();
 				if (numTopics > 0) {
-					var text = this.renderTemplate("emailNotification", {
+					var text = this.renderTemplate('emailNotification', {
 						username: username,
 						nodes: nodes
 					});
 					try {
 						var mail = new Mail();
-						mail.setFrom(getProperty("serverEmail"));
+						mail.setFrom(getProperty('serverEmail'));
 						mail.setTo(username + ' <' + email + '>');
-						mail.setSubject("// Scriptographer.com: Discussion Notification //");
+						mail.setSubject(getProperty('notificationTitle'));
 						mail.addPart(text);
 						mail.send();
 					} catch (e) {
-						logError("checkNotifications" , e);
+						logError('checkNotifications' , e);
 					}
 				}
 			}
