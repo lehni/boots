580d3f04c410b0b7d23949305e29eb5e068912ae
Jürg Lehni <juerg@scratchdisk.com>
2009-07-21 22:41:01 +0000

    Corrected conversion scripts and sql and fixed a transition bug.

---
+++ convert.sql
@@ -0,0 +1,66 @@
+# 1.
+
+ALTER TABLE nodes ADD visible TINYINT UNSIGNED NULL AFTER name;
+ALTER TABLE topics ADD visible TINYINT UNSIGNED NULL AFTER name;
+ALTER TABLE resources ADD visible TINYINT UNSIGNED NULL AFTER name;
+
+UPDATE nodes SET visible = 1 WHERE position IS NOT NULL;
+UPDATE topics SET visible = 1 WHERE position IS NOT NULL;
+UPDATE resources SET visible = 1 WHERE position IS NOT NULL;
+
+# all resources that are attached to nodes should be visible (only thumbnails for now)
+UPDATE resources SET visible = 1 WHERE topic_id IS NOT NULL;
+
+#UPDATE nodes SET visible = 1;
+#UPDATE topics SET visible = 1;
+#UPDATE resources SET visible = 1;
+
+#UPDATE nodes SET position = null where visible <=> 1;
+#UPDATE topics SET position = null where visible <=> 1;
+#UPDATE resources SET position = null where visible <=> 1;
+
+#ALTER TABLE nodes DROP visible;
+#ALTER TABLE topics DROP visible;
+#ALTER TABLE resources DROP visible;
+
+# 2. add related_ids to general nodes
+
+ALTER TABLE nodes ADD related_ids VARCHAR( 255 ) NULL AFTER modification_date;
+
+# 3. before execution of convert
+
+ALTER TABLE posts ADD node_id INT UNSIGNED NULL AFTER topic_id;
+ALTER TABLE posts DROP INDEX `index` , ADD INDEX `index` ( `node_id` , `is_first` );
+
+ALTER TABLE resources ADD node_id INT UNSIGNED NULL AFTER topic_id;
+
+ALTER TABLE notifications ADD node_id INT UNSIGNED NULL AFTER topic_id;
+
+ALTER TABLE users CHANGE `role` `roles` INT( 10 ) UNSIGNED NULL DEFAULT NULL;
+
+# 4. execute scriptographer/convert to convert topic -> node
+
+# 5. after execution of convert
+
+ALTER TABLE posts DROP topic_id;
+ALTER TABLE resources DROP topic_id;
+ALTER TABLE notifications DROP topic_id;
+DROP TABLE topics;
+
+# 6. resources
+
+ALTER TABLE resources CHANGE string type VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
+
+# 7. remove RefMember nodes and posts
+
+DELETE posts FROM posts, nodes WHERE posts.node_id = nodes.id AND nodes.prototype = 'RefMember';
+DELETE FROM nodes WHERE prototype = 'RefMember';
+
+# 8. title / name
+ALTER TABLE nodes ADD title VARCHAR( 64 ) NULL AFTER related_ids;
+UPDATE nodes SET title = name;
+# lower case names
+UPDATE nodes SET name = LOWER(name);
+
+# 9. empty search
+TRUNCATE TABLE search;
--- post/helma/Root/convert.js
+++ post/helma/Root/convert.js
@@ -1,17 +1,38 @@
 Root.inject({
 	convert_action: function() {
 		if (User.canEdit(this)) {
-			var con = getDBConnection("main");
-			var nodeId = con.executeRetrieval("SELECT MAX(id) + 1 FROM nodes;");
+			var con = getDBConnection('main');
+			// User role flags
+			var users = con.executeRetrieval('SELECT id, roles FROM users');
+			while (users.next()) {
+				var id = users.getColumnItem('id');
+				var roles = users.getColumnItem('roles');
+				var oldRoles = {
+					1: User.READER,
+					2: User.EDITOR | User.POSTER,
+					4: User.ADMINISTRATOR,
+					8: User.SUPERUSER,
+					16: User.DISABLED,
+					32: User.UNVERIFIED
+				};
+				var newRoles = 0;
+				oldRoles.each(function(newFlag, oldFlag) {
+					if (roles & oldFlag)
+						newRoles |= newFlag;
+				});
+				con.executeCommand('UPDATE users SET roles = ' + newRoles + ' WHERE id = ' + id + ';');
+			}
+			// Topics
+			var nodeId = con.executeRetrieval('SELECT MAX(id) + 1 FROM nodes;');
 			if (nodeId.next())
 				nodeId = parseInt(nodeId.getColumnItem(1));
-			var topics = con.executeRetrieval("SELECT id, prototype, name, visible, position, node_id, CAST(creator_id AS CHAR) AS creator_id, creation_date, CAST(modifier_id AS CHAR) AS modifier_id, modification_date, topic_ids, CAST(int_1 AS CHAR) AS int_1 FROM topics");
-			var dateFormat = "yyyy-MM-dd hh:mm:ss";
+			var topics = con.executeRetrieval('SELECT id, prototype, name, visible, position, node_id, CAST(creator_id AS CHAR) AS creator_id, creation_date, CAST(modifier_id AS CHAR) AS modifier_id, modification_date, topic_ids, CAST(int_1 AS CHAR) AS int_1 FROM topics');
+			var dateFormat = 'yyyy-MM-dd hh:mm:ss';
 			while (topics.next()) {
-				var creator = topics.getColumnItem("creator_id"), modifier = topics.getColumnItem("modifier_id");
-				var creation = topics.getColumnItem("creation_date"), modification = topics.getColumnItem("modification_date");
-				var related = topics.getColumnItem("topic_ids");
-				var int_1 = topics.getColumnItem("int_1");
+				var creator = topics.getColumnItem('creator_id'), modifier = topics.getColumnItem('modifier_id');
+				var creation = topics.getColumnItem('creation_date'), modification = topics.getColumnItem('modification_date');
+				var related = topics.getColumnItem('topic_ids');
+				var int_1 = topics.getColumnItem('int_1');
 				var query = 'INSERT into nodes (id, prototype, name, visible, position, parent_id, creator_id, creation_date, modifier_id, modification_date, related_ids, int_1) VALUES(' +
 					'"' + nodeId + '", ' +
 					'"' + topics.getColumnItem("prototype") + '", ' +
@@ -31,7 +52,7 @@ Root.inject({
 				var error = con.getLastError();
 				if (error)
 					res.write(error);
-				var topicId = topics.getColumnItem("id");
+				var topicId = topics.getColumnItem('id');
 				con.executeCommand('UPDATE posts SET node_id = ' + nodeId + ' WHERE topic_id = ' + topicId + ';');
 				con.executeCommand('UPDATE notifications SET node_id = ' + nodeId + ' WHERE topic_id = ' + topicId + ';');
 				con.executeCommand('UPDATE resources SET node_id = ' + nodeId + ' WHERE topic_id = ' + topicId + ';');
