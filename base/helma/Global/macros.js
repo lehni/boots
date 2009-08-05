bf5003b030fe16f61e2dc671ef939e25d8e7ebb4
Jürg Lehni <juerg@scratchdisk.com>
2009-08-05 02:15:53 +0000

    Moved Html.js macros to macros.js.

--- base/helma/Global/macros.js
+++ base/helma/Global/macros.js
@@ -1,3 +1,41 @@
+function input_macro(param) {
+	if (param.name) {
+		var value = req.data[param.name];
+		if (value) {
+			value = value.toString();
+			if (param.type == 'radio' || param.type == 'checkbox')
+				param.current = value;
+			else param.value = value;
+		}
+	}
+	Html.input(param, res);
+}
+
+function textarea_macro(param) {
+	Html.textarea(param, res);
+}
+
+function select_macro(param) {
+	Html.select(param, res);
+}
+
+function script_macro(param) {
+	var lastModified = Net.getLastModified(param.src);
+	if (lastModified)
+		param.src += '?' + lastModified;
+	Html.script(param, res);
+}
+
+/* This clashes with the global link macro. TODO: Rename the link macro
+ to something else, e.g.g anchor, a, etc.?
+function link_macro(param) {
+	var lastModified = Net.getLastModified(param.href);
+	if (lastModified)
+		param.href += '?' + lastModified;
+	Html.link(param, res);
+}
+*/
+
 function json_macro(param, object) {
 	// If param.include is defined, only include the keys defined in that
 	// coma seperated string:
@@ -40,4 +78,8 @@ function random_macro(param, first, second) {
 		res.write(Math.rand(first, second));
 	else
 		res.write(Math.random());
+}
+ 
+// dummy macro named __ in order to create comments in skins like this: <%__ comment %>
+function ___macro(param) {
 }
