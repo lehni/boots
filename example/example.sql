746e3ed39dfd87ea7a1f3dcb1e14461a0eee7a1d
Jürg Lehni <juerg@scratchdisk.com>
2009-09-09 13:54:38 +0000

    Updated example sql.

--- example/example.sql
+++ example/example.sql
@@ -2,20 +2,20 @@ GRANT CREATE,ALTER,SELECT,INSERT,UPDATE,DELETE ON example.* TO example@localhost
 
 DROP TABLE IF EXISTS nodes;
 CREATE TABLE nodes (
-  id int(10) unsigned NOT NULL default '0',
-  prototype varchar(255) default NULL,
-  name varchar(255) default NULL,
-  title varchar(255) default NULL,
-  visible tinyint(3) unsigned default NULL,
-  position tinyint(3) unsigned default NULL,
-  parent_id int(10) unsigned default NULL,
-  creator_id int(11) default '0',
-  creation_date datetime default NULL,
-  modifier_id int(11) default '0',
-  modification_date datetime default NULL,
-  related_ids varchar(255) default NULL,
+  id int(10) unsigned NOT NULL DEFAULT '0',
+  prototype varchar(255) DEFAULT NULL,
+  name varchar(255) DEFAULT NULL,
+  title varchar(255) DEFAULT NULL,
+  visible tinyint(3) unsigned DEFAULT NULL,
+  position tinyint(3) unsigned DEFAULT NULL,
+  parent_id int(10) unsigned DEFAULT NULL,
+  creator_id int(11) DEFAULT NULL,
+  creation_date datetime DEFAULT NULL,
+  modifier_id int(11) DEFAULT NULL,
+  modification_date datetime DEFAULT NULL,
+  related_ids varchar(255) DEFAULT NULL,
   text text,
-  PRIMARY KEY  (id),
+  PRIMARY KEY (id),
   KEY name (name,text(64)),
   FULLTEXT KEY search (name,text)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
@@ -24,42 +24,42 @@ INSERT INTO nodes VALUES (0,'Root','welcome','Welcome',1,0,NULL,0,'2007-11-06 15
 
 DROP TABLE IF EXISTS resources;
 CREATE TABLE resources (
-  id int(10) unsigned NOT NULL default '0',
-  node_id int(10) unsigned default NULL,
-  post_id int(10) unsigned default NULL,
-  prototype varchar(255) default NULL,
-  name varchar(255) default NULL,
-  visible tinyint(3) unsigned default NULL,
-  position tinyint(3) unsigned default NULL,
-  creator_id int(11) default '0',
-  creation_date datetime default NULL,
-  modifier_id int(11) default '0',
-  modification_date datetime default NULL,
-  extension varchar(16) default NULL,
-  counter int(10) unsigned default NULL,
-  version int(10) unsigned default NULL,
-  width smallint(5) unsigned default NULL,
-  height smallint(5) unsigned default NULL,
-  type varchar(255) default NULL,
-  PRIMARY KEY  (id),
+  id int(10) unsigned NOT NULL DEFAULT '0',
+  node_id int(10) unsigned DEFAULT NULL,
+  post_id int(10) unsigned DEFAULT NULL,
+  prototype varchar(255) DEFAULT NULL,
+  name varchar(255) DEFAULT NULL,
+  visible tinyint(3) unsigned DEFAULT NULL,
+  position tinyint(3) unsigned DEFAULT NULL,
+  creator_id int(11) DEFAULT NULL,
+  creation_date datetime DEFAULT NULL,
+  modifier_id int(11) DEFAULT NULL,
+  modification_date datetime DEFAULT NULL,
+  extension varchar(16) DEFAULT NULL,
+  counter int(10) unsigned DEFAULT NULL,
+  version int(10) unsigned DEFAULT NULL,
+  width smallint(5) unsigned DEFAULT NULL,
+  height smallint(5) unsigned DEFAULT NULL,
+  type varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id),
   KEY name (name)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-INSERT INTO resources VALUES (1,3,NULL,'Picture','Happy_Birds.jpg',1,0,0,'2007-11-08 00:14:40',0,'2008-04-07 15:57:11','jpg',0,NULL,500,358,'image');
+INSERT INTO resources VALUES (1,3,NULL,'Picture','Happy_Birds.jpg',1,0,0,'2007-11-08 00:14:40',0,'2008-04-07 17:03:39','jpg',0,NULL,500,358,'image');
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-  id int(10) unsigned NOT NULL default '0',
-  name varchar(255) default NULL,
-  full_name varchar(64) default NULL,
-  password varchar(255) default NULL,
-  roles int(10) unsigned default NULL,
-  email varchar(255) default NULL,
-  registration_date datetime default NULL,
-  last_login datetime default NULL,
+  id int(10) unsigned NOT NULL DEFAULT '0',
+  name varchar(255) DEFAULT NULL,
+  full_name varchar(64) DEFAULT NULL,
+  password varchar(255) DEFAULT NULL,
+  roles int(10) unsigned DEFAULT NULL,
+  email varchar(255) DEFAULT NULL,
+  registration_date datetime DEFAULT NULL,
+  last_login datetime DEFAULT NULL,
   text text,
-  website varchar(255) default NULL,
-  PRIMARY KEY  (id),
+  website varchar(255) DEFAULT NULL,
+  PRIMARY KEY (id),
   KEY name (name)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
