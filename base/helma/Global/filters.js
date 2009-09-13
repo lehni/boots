f617170819748e76a687b987d831ee6409b07059
Jürg Lehni <juerg@scratchdisk.com>
2009-09-13 17:28:37 +0000

    Cleaned up filters and added one for url based file versioning.

--- base/helma/Global/filters.js
+++ base/helma/Global/filters.js
@@ -1,71 +1,77 @@
 function lowercase_filter(input) {
-	return input && input.toString().toLowerCase();
+	return input != null && input.toString().toLowerCase();
 }
 
 function uppercase_filter(input) {
-	return input && input.toString().toUpperCase();
+	return input != null && input.toString().toUpperCase();
 }
 
 function capitalize_filter(input) {
-	return input && input.toString().capitalize();
+	return input != null && input.toString().capitalize();
 }
 
 function camelize_filter(input, param, separator) {
-	return input && input.toString().camelize(param.separator || separator);
+	return input != null && input.toString().camelize(
+			param.separator || separator
+	);
 }
 
 function uncamelize_filter(input, param, separator) {
-	return input && input.toString().uncamelize(param.separator || separator);
+	return input != null && input.toString().uncamelize(
+			param.separator || separator
+	);
 }
 
 function hyphenate_filter(input, param, separator) {
-	return input && input.toString().hyphenate(param.separator || separator);
+	return input != null && input.toString().hyphenate(
+			param.separator || separator
+	);
 }
 
 function trim_filter(input) {
-	return input && input.toString().trim();
+	return input != null && input.toString().trim();
 }
 
 function unaccent_filter(input) {
-	return input && input.toString().unaccent();
+	return input != null && input.toString().unaccent();
 }
 
 function urlize_filter(input) {
-	return input && input.toString().urlize();
+	return input != null && input.toString().urlize();
 }
 
 function truncate_filter(input, param, limit, suffix) {
-	return input && input.toString().truncate(
-		param.limit != null ? param.limit : limit,
-		param.suffix || suffix
+	return input != null && input.toString().truncate(
+			Base.pick(param.limit, limit),
+			param.suffix || suffix
 	);
 }
 
 function wordwrap_filter(input, param, width) {
-	return input && input.toString().wordwrap(
-		param.width != null ? param.width : width
+	return input != null && input.toString().wordwrap(
+			Base.pick(param.width, width)
 	);
 }
 
 function stripTags_filter(input) {
-	return input && stripTags(input.toString());
+	return input != null && stripTags(input.toString());
 }
 
 function breaksToHtml_filter(input) {
-	return input && input.replaceAll('\n', '<br />');
+	return input != null && input.replaceAll('\n', '<br />');
 }
 
 function replace_filter(input, param, search, replace) {
-	return input && input.replaceAll(
-		param.search != null ? param.search : search,
-		param.replace != null ? param.replace : replace
+	return input != null && input.replaceAll(
+			Base.pick(param.search, search),
+			Base.pick(param.replace, replace)
 	);
 }
 
 function substring_filter(input, param, from, to) {
-	return input && input.substring(
-		param.from != null ? param.from : from,
-		param.to != null ? param.to : to
+	return input != null && input.substring(
+			Base.pick(param.from, from),
+			Base.pick(param.to, to)
 	);
 }
 
@@ -74,9 +80,19 @@ function substring_filter(input, param, from, to) {
  * supported too.
  */
 function format_filter(input, param, format, locale) {
-	return input != null && input.format(param.format || format, param.locale || locale);
+	return input != null && input.format(
+			param.format || format,
+			param.locale || locale
+	);
 }
 
 function markup_filter(input, param) {
 	return Markup.render(input, param);
+}
+
+function versioned_filter(input, param) {
+	var lastModified = Net.getLastModified(input);
+	if (lastModified)
+		input += '?' + lastModified;
+	return input;
 }
