4f77c20b63f92ec6be7c6dd198bf7bdd1edaf596
Jürg Lehni <juerg@scratchdisk.com>
2010-01-17 16:18:06 +0000

    Amended encodeAttributes that seems obscure.

--- base/helma/Global/encode.js
+++ base/helma/Global/encode.js
@@ -50,6 +50,8 @@ function decodeBase64(str) {
 
 /**
  * Encodes strings for html attributes, replacing quotes with their hex values
+ * TODO: This appears to not be for attributes but scripts within attributes?
+ * Find out more... Wouldn't a normal encode work for this? It does hor input values...
  */
 function encodeAttribute(str, singles) {
 	return str.replace(singles ? /'/gm : /"/gm, function(match) { // '
