13ebf806523684e3e8643d59cd58d847089b303c
Jürg Lehni <juerg@scratchdisk.com>
2010-01-20 16:40:04 +0000

    Clean-up.

--- type/helma/Page/functions.js
+++ type/helma/Page/functions.js
@@ -2,8 +2,7 @@ Page.inject({
 	getEditForm: function(param) {
 		if (param.title === undefined)
 			param.title = true;
-		var form = this.base(param);
-		return form;
+		return this.base(param);
 	},
 
 	getDisplayName: function() {
