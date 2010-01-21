9c33a33bdc11d83d097a0d290ac93980860df29d
Jürg Lehni <juerg@scratchdisk.com>
2010-01-21 00:07:28 +0000

    Commented code tag.

--- base/helma/Global/markup/CodeTag.js
+++ base/helma/Global/markup/CodeTag.js
@@ -2,10 +2,15 @@ CodeTag = MarkupTag.extend({
 	_tags: 'code',
 
 	render: function(content, param, encoder, before, after) {
-		var code = '<code>' + encoder(content.replace(/<br\s*\/?>/g, '')) + '</code>';
+		var code = content.replace(/<br\s*\/?>/g, '');
+		// Nesting things in <code> seems to cause conflicts with Helma's
+		// encodeParagraphs that is somehow too smart about it, so use <pre>
+		// for blocks of code and <tt> for inlined code.
 		if ((typeof before != 'string' || /[\n\r]$/.test(before))
-				&& (typeof after != 'string' || /^[\r\n]/.test(after)))
-			code = '<pre>' + code + '</pre>';
-		return code;
+				&& (typeof after != 'string' || /^[\r\n]/.test(after))) {
+			return '<pre>' + code + '</pre>';
+		} else {
+			return '<tt>' + code + '</tt>';
+		}
 	}
 });
