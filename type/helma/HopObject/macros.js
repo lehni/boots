df23bed11688901c0b25766303d0e52d542d5674
Jürg Lehni <juerg@scratchdisk.com>
2010-01-28 23:00:04 +0000

    Added ifNotVisiting parameter to renderLink, so links are only rendered if the object is not already the last element in the path.

--- type/helma/HopObject/macros.js
+++ type/helma/HopObject/macros.js
@@ -2,6 +2,12 @@ HopObject.inject({
 	link_macro: function(param, content) {
 		if (content != null)
 			param.content = content;
-		this.renderLink(param, res);
+		if (param.ifNotVisiting && path[path.length - 1] == this) {
+			// TODO: Template.js seems broken as we cannot return a value to
+			// be rendered from macros.
+			res.encode(param.content || this.getDisplayName());
+		} else {
+			this.renderLink(param, res);
+		}
 	}
 });
