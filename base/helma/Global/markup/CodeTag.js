01bafb03d91d9ccae49fff05ec676f5a4ce5aac9
Jürg Lehni <juerg@scratchdisk.com>
2010-01-28 23:01:37 +0000

    Fixed code tag context awareness to work at beginning and end of text too.

--- base/helma/Global/markup/CodeTag.js
+++ base/helma/Global/markup/CodeTag.js
@@ -6,8 +6,8 @@ CodeTag = MarkupTag.extend({
 		// Nesting things in <code> seems to cause conflicts with Helma's
 		// encodeParagraphs that is somehow too smart about it, so use <pre>
 		// for blocks of code and <tt> for inlined code.
-		if ((typeof before != 'string' || /[\n\r]$/.test(before))
-				&& (typeof after != 'string' || /^[\r\n]/.test(after))) {
+		if ((typeof before != 'string' || /[\n\r]$|^$/.test(before))
+				&& (typeof after != 'string' || /^[\r\n]|^$/.test(after))) {
 			return '<pre>' + code + '</pre>';
 		} else {
 			return '<tt>' + code + '</tt>';
