ae74882d63580c73982bf471bd7fcb992ef2be31
Jürg Lehni <juerg@scratchdisk.com>
2010-04-22 01:26:40 +0000

    Cleaned up toRender() function to use standard Function#length instead of removed Function#getParameters().length.

--- base/helma/Global/01_Function.js
+++ base/helma/Global/01_Function.js
@@ -6,33 +6,25 @@ Function.inject({
 	 * used to render to a string and return it.
 	 * Otherwise, the function renders directly into res.
 	 */
-
-	toRender: function(count) {
-		var length = count == undefined ? this.getParameters().length : count;
+	toRender: function() {
+		var length = this.length;
 		var last = length ? length - 1 : 0;
 		var that = this;
 		return function() {
-			// look at the last argument. if it's null, 
+			// Look at the last argument. if it's null, 
 			// render into a string.
 			var asString = !arguments[last];
 			if (asString) {
 				res.push();
-				// this seems to execute faster than the creation
+				// This seems to execute faster than the creation
 				// of a new array, and at least in Rhino, it works!
 				arguments[last] = res;
 				arguments.length = length;
 			}
 			that.apply(this, arguments);
-			// return the string if required
+			// Return the string if required
 			if (asString)
 				return res.pop();
-		}
-	},
-
-	statics: {
-		compile: function(params, body) {
-			return Packages.org.mozilla.javascript.Context.getCurrentContext().compileFunction(
-				{}, 'function (' + params + ') {' + body + '}', 'Function', 0, null);
 		}
 	}
 });
