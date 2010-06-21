712fd43e33ca036f02e384199e24a8a18b5a1b6d
Jürg Lehni <juerg@scratchdisk.com>
2010-06-21 14:33:08 +0000

    Clean up example code and fix removed hasLinks property.

--- example/helma/Page/functions.js
+++ example/helma/Page/functions.js
@@ -1,7 +1,12 @@
 Page.inject({
 	getEditForm: function(param) {
 		var form = this.base(param);
-		form.addTab("Content", { label: "Text", name: "text", type: "text", cols: 45, rows: 20, hasLinks: true });
+		form.addTab('Content', {
+			label: 'Text', name: 'text', type: 'text', cols: 45, rows: 20,
+			buttons: {
+				links: true
+			}
+		});
 		return form;
 	}
 });
--- example/helma/User/functions.js
+++ example/helma/User/functions.js
@@ -1,12 +1,15 @@
 User.inject({
 	getEditForm: function(param) {
 		var form = this.base(param);
-		form.insertAfter("password", {
-			label: "Full Name", name: "fullName", type: "string", length: 16
+		form.insertAfter('password', {
+			label: 'Full Name', name: 'fullName', type: 'string', length: 16
 		}, {
-			label: "Website", name: "website", type: "string", length: 255
+			label: 'Website', name: 'website', type: 'string', length: 255
 		}, {
-			label: "Text", name: "text", type: "text", cols: 45, rows: 5, hasLinks: true
+			label: 'Text', name: 'text', type: 'text', cols: 45, rows: 5,
+			buttons: {
+				links: true
+			}
 		});
 		return form;
 	}
