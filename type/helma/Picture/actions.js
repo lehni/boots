12139de66b4cb3f67bfcee86a68f0f5f21c4a4cf
Jürg Lehni <juerg@scratchdisk.com>
2010-12-23 11:39:28 +0000

    Convert main_action function definition to using inject().

--- type/helma/Picture/actions.js
+++ type/helma/Picture/actions.js
@@ -1,24 +1,26 @@
-function main_action() {
-	if (/\/$/.test(req.path)) { // Path ends with / -> we're in a popup
-		this.renderPopup({
-			content: this.renderTemplate('picture', {
-				// Make sure we're rendering the popup image versioned even if
-				// the site is not using versioning, as it will show in a
-				// different size.
-				picture: this.renderImage(Hash.append({ versioned: true },
-					this.MAX_POPUP_SIZE))
-			})
-		}, res);
-	} else { // Just the file
-		// If we're using versioning, make sure it's a valid thumbnail id:
-		// If not, we're still allowing one version of the file, which is
-		// just however the site is rendering it. The side needs to make
-		// sure it is not rendering it in two different sizes then.
-		if (!this.VERSIONED || /[a-z0-9]{32}/.test(req.data.v)) {
-			var file = this.getVersionFile(req.data.v);
-			if (file.exists())
-				this.forwardFile(file);
+Picture.inject({
+	main_action: function() {
+		if (/\/$/.test(req.path)) { // Path ends with / -> we're in a popup
+			this.renderPopup({
+				content: this.renderTemplate('picture', {
+					// Make sure we're rendering the popup image versioned even if
+					// the site is not using versioning, as it will show in a
+					// different size.
+					picture: this.renderImage(Hash.append({ versioned: true },
+						this.MAX_POPUP_SIZE))
+				})
+			}, res);
+		} else { // Just the file
+			// If we're using versioning, make sure it's a valid thumbnail id:
+			// If not, we're still allowing one version of the file, which is
+			// just however the site is rendering it. The side needs to make
+			// sure it is not rendering it in two different sizes then.
+			if (!this.VERSIONED || /[a-z0-9]{32}/.test(req.data.v)) {
+				var file = this.getVersionFile(req.data.v);
+				if (file.exists())
+					this.forwardFile(file);
+			}
+			this.forwardFile();
 		}
-		this.forwardFile();
 	}
-}
+});
