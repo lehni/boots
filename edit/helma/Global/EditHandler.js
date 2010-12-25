0cac3e5d1486c81fbcc481e5180290ad255c7bb3
Jürg Lehni <juerg@scratchdisk.com>
2010-12-25 14:34:38 +0000

    Reformat code for better readability.

--- edit/helma/Global/EditHandler.js
+++ edit/helma/Global/EditHandler.js
@@ -1,6 +1,74 @@
 EditHandler = Base.extend(new function() {
 	var handlers = new Hash();
 
+	function handle(handler, node, base, mode) {
+		// Call the handler and commit changes if there are any.
+		// Make sure a new form is produced each time when editing.
+		var form = node.getForm(mode == 'edit');
+		// Retrieve item too if defined and pass it to the handler.
+		var item = req.data.edit_item !== undefined
+				? form.getItem(req.data.edit_item, req.data.edit_group)
+				: null;
+		// If the item could not be found and edit_entry_id is set, the item is
+		// to be found in a editable list entry.
+		if (item == null && req.data.edit_entry_id) {
+			// Find the form through the entry edit node, and the item from
+			// there.
+			var entryNode = EditNode.get(req.data.edit_entry_id);
+			// Do not override node with entryNode, as editable list entries do
+			// not have nodes, they are inlined.
+			var entryForm = entryNode
+					&& entryNode.getForm(mode == 'edit');
+			if (entryForm) {
+				item = entryForm.getItem(req.data.edit_item,
+						req.data.edit_group);
+			}
+		}
+		var oldHref = base.href();
+		var result = handler.handle(base, node.object, node, form, item);
+		if (result == EditForm.COMMIT) {
+			res.commit();
+			var redirect = null;
+			if (base.isTransient()) {
+				// The object has been removed in the meantime, redirect to its
+				// parent.
+				redirect = base.getParent();
+				if (!redirect) {
+					// Find parent in path:
+					for (var i = path.length - 1; i > 0; i--) {
+						var obj = path[i];
+						if (obj == base) {
+							redirect = path[i - 1];
+							break;
+						}
+					}
+				}
+			} else if (base.href() != oldHref) {
+				// A renaming has lead to a changing href. We need to redirect
+				// to the new place in order to be able to continue editing.
+				redirect = base;
+			}
+			if (redirect) {
+				editResponse.redirect = redirect.href();
+			} else {
+				// Render the updated html and cause edit.js to update on the
+				// fly. See if there is a render_action on the object.
+				var render = EditForm.ACTION_RENDER
+					&& base[EditForm.ACTION_RENDER + '_action'];
+				if (render) {
+					// Render the page into editResponse.page:
+					res.push();
+					render.call(base);
+					editResponse.page = res.pop();
+				}
+			}
+		} else if (result == EditForm.NOT_ALLOWED) {
+			EditForm.setMessage('editNotAllowed');
+			return false;
+		}
+		return true;
+	}
+
 	return {
 		statics: {
 			extend: function(src) {
@@ -24,109 +92,44 @@ EditHandler = Base.extend(new function() {
 				if (app.properties.debugEdit) {
 					User.log('Edit Request:', fullId, "'" + mode + "'");
 					var values = [];
+					var filter = /^(http_host|HopSession|http_language|http_remotehost|autoLogin|http_browser|http_referer)$/;
 					for (var i in req.data)
-						if (!/^(http_host|HopSession|http_language|http_remotehost|autoLogin|http_browser|http_referer)$/.test(i))
+						if (!filter.test(i))
 							values.push(i + ': ' + Json.encode(req.data[i]));
 					User.log('Edit Data:\n' + values.join('\n'));
 				}
 				var handler = handlers[mode];
 				var out = null;
-				// Only allow editing modes if the edi stack is valid.
-				// Otherwise it just renders the edit form again and ignores
-				// the editing step.
+				// Only allow editing modes if the edi stack is valid. Otherwise
+				// it just renders the edit form again and ignores the editing
+				// step.
 				var handled = true;
 				if (req.data.helma_upload_error) {
 					EditForm.alert(req.data.helma_upload_error);
 					User.log('Upload Error', req.data.helma_upload_error);
 				} else if (handler) {
 					res.push();
-					var commited = false;
 					try {
 						var node = EditNode.get(fullId);
-						// check again that we have the rights to edit:
 						if (node) {
-							// Call the handler and commit changes if there are any
+							// Call the handler and commit changes if there are.
 							node.log(mode);
-							// Make sure a new form is produced each time when editing
-							var form = node.getForm(mode == 'edit');
-							// Retrieve item too if defined and pass it to handler
-							var item = req.data.edit_item !== undefined
-								? form.getItem(req.data.edit_item, req.data.edit_group)
-								: null;
-							// If the item could not be found and edit_entry_id is
-							// set, the item is to be found in a editable list entry.
-							if (item == null && req.data.edit_entry_id) {
-								// Find the form through the entry edit node, and
-								// the item from there.
-								var entryNode = EditNode.get(req.data.edit_entry_id);
-								// Do not override node with entryNode, as editable
-								// list entries do not have nodes, they are inlined.
-								var entryForm = entryNode && entryNode.getForm(mode == 'edit');
-								if (entryForm)
-									item = entryForm.getItem(req.data.edit_item, req.data.edit_group);
-							}
-							var oldHref = base.href();
-							var result = handler.handle(base, node.object, node, form, item);
-							if (result == EditForm.COMMIT) {
-								commited = true;
-								res.commit();
-								var redirect = null;
-								if (base.isTransient()) {
-									// The object has been removed in the meantime
-									// Redirect to its parent.
-									redirect = base.getParent();
-									if (!redirect) {
-										// Find parent in path:
-										for (var i = path.length - 1; i > 0; i--) {
-											var obj = path[i];
-											if (obj == base) {
-												redirect = path[i - 1];
-												break;
-											}
-										}
-									}
-								} else if (base.href() != oldHref) {
-									// A renaming has lead to a changing href. We
-									// need to redirect to the new place in order
-									// to be able to continue editing.
-									redirect = base;
-								}
-								if (redirect) {
-									editResponse.redirect = redirect.href();
-								} else {
-									// Render the updated html and cause edit.js
-									// to update on the fly.
-									// See if there is a render_action on the object
-									var render = EditForm.ACTION_RENDER
-										&& base[EditForm.ACTION_RENDER + '_action'];
-									if (render) {
-										// Render the page into editResponse.page:
-										res.push();
-										render.call(base);
-										editResponse.page = res.pop();
-									}
-								}
-							} else if (result == EditForm.NOT_ALLOWED) {
-								EditForm.setMessage('editNotAllowed');
-								handled = false;
-							}
+							handled = handle(handler, node, base, mode);
 						}
 					} catch (e) {
 						EditForm.reportError(e);
-						// Rollback changes in case there was an exception
-						// in res.commit() above, as otherwise the same error
-						// will be thrown again at the end of the response
-						// handling in the automatic commit() call. This is
-						// required to make sure error messages get through to
-						// the client.
-						if (commited)
-							res.rollback();
+						// Rollback changes in case there was an exception in
+						// res.commit() above, as otherwise the same error  will
+						// be thrown again at the end of the response handling
+						// in the automatic commit() call. This is required to
+						// make sure error messages get through to the client.
+						res.rollback();
 					}
-					var out = res.pop();
-					// Assume that if id was not set yet, no form was renderet yet
-					// And we should render the current one. This allows the handlers
-					// to be lazy about rendering of the current form, e.g. when
-					// the ApplyHandler receives an exception.
+					out = res.pop();
+					// Assume that if id was not set yet, no form was rendered
+					// yet, and we should render the current one. This allows
+					// the handlers to be lazy about rendering of the current
+					// form, e.g. when the ApplyHandler receives an exception.
 					if (!out && !editResponse.id) {
 						// Go back the given amount, if any
 						if (req.data.edit_back) {
@@ -155,19 +158,24 @@ EditHandler = Base.extend(new function() {
 					delete editResponse.added;
 					out = Json.encode(editResponse);
 				}
-				// Prevent any caching at the remote server or any intermediate proxy 
-				// Tested on most browsers with http://www.mnot.net/javascript/xmlhttprequest/cache.html
-				res.servletResponse.setHeader('Cache-Control', 'no-cache,max-age=0');
+				// Prevent any caching at the remote server or any intermediate
+				// proxy. Tested on most browsers with:
+				// http://www.mnot.net/javascript/xmlhttprequest/cache.html
+				res.servletResponse.setHeader('Cache-Control',
+						'no-cache,max-age=0');
 				if (/^multipart\/form-data/.test(req.servletRequest.contentType)
 						&& res.contentType == 'text/javascript') {
-					// Unfortunately an iframe response needs to be inside a textarea...
+					// Unfortunately an iframe response needs to be inside a
+					// textarea...
 					res.contentType = 'text/html';
-					res.write('<html><body><textarea>' + encodeEntities(out) + '</textarea></body></html>')
+					res.write('<html><body><textarea>' + encodeEntities(out)
+							+ '</textarea></body></html>');
 				} else {
 					res.write(out);
 				}
-//				if (app.properties.debugEdit)
-					User.log('EditHandler.handle() time: ', Date.now() - startTime);
+				if (app.properties.debugEdit)
+					User.log('EditHandler.handle() time: ',
+							Date.now() - startTime);
 				return handled;
 			},
 
