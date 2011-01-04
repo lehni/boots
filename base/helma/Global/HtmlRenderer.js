4545ed5112458433f80e08cb50aa33aab9c5dbfb
Jürg Lehni <juerg@scratchdisk.com>
2011-01-04 16:53:20 +0000

    Improve HtmlRenderer to receive all parameters from one param object.

--- base/helma/Global/HtmlRenderer.js
+++ base/helma/Global/HtmlRenderer.js
@@ -1,5 +1,5 @@
 HtmlRenderer = Base.extend({
-	initialize: function(stylesheet) {
+	initialize: function() {
 		// Create a HTMLEditorKit that turns of asynchronous loading:
 		this.kit = new JavaAdapter(javax.swing.text.html.HTMLEditorKit, {
 			createDefaultDocument: function() {
@@ -9,54 +9,62 @@ HtmlRenderer = Base.extend({
 				return doc;
 			}
 		});
-		if (stylesheet) {
-			var ss = new javax.swing.text.html.StyleSheet();
-			var reader = new java.io.BufferedReader(
-					new java.io.FileReader(stylesheet));
-			ss.loadRules(reader, null);
-			reader.close();
-			this.kit.setStyleSheet(ss);
-		}
-		this.margin = new java.awt.Insets(0, 0, 0, 0);
 	},
 
-	render: function(src, width, height) {
+	render: function(param) {
 		var img = null;
 		try {
 			var pane = new Packages.javax.swing.JEditorPane();
-			pane.setEditorKit(this.kit);
 			pane.setEditable(false);
-			pane.setMargin(this.margin);
+			// Needed?
+			pane.setMargin(new java.awt.Insets(0, 0, 0, 0));
+			pane.setEditorKit(this.kit);
 
-			// See if src is a URL, otherwise treat it as HTML code
-			try {
-				var url = new java.net.URL(src);
-				pane.setPage(url);
-			} catch (e) {
+			if (param.stylesheet) {
+				var ss = new javax.swing.text.html.StyleSheet();
+				var reader = new java.io.BufferedReader(
+						new java.io.FileReader(param.stylesheet));
+				ss.loadRules(reader, null);
+				reader.close();
+				this.kit.setStyleSheet(ss);
+			}
+
+			if (param.url) {
+				pane.setPage(new java.net.URL(param.url));
+			} else if (param.html) {
 				pane.setContentType('text/html');
 				// <br/> and so on is not supported:
-				src = src.replaceAll('/>', '>');
-				pane.setText(src);
+				pane.setText(param.html.replaceAll('/>', '>'));
 			}
+
+			var width = param.width, height = param.height;
 			// Determine width
-			if (width == -1) {
+			if (!width) {
 				var size = pane.getPreferredSize();
 				width = size.width;
 			}
 			// Determine height
-			if (height == -1) {
+			if (!height) {
 				// Height depends on width
 				pane.setSize(width, 1);
 				var size = pane.getPreferredSize();
 				height = size.height;
 			}
-			img = new Image(width, height);
-			var g = img.getGraphics();
-			Packages.javax.swing.SwingUtilities.paintComponent(g, pane,
-					new java.awt.Container(), 0, 0, width, height);
-			g.dispose();
+			if (param.minHeight && height < param.minHeight) {
+				height = param.minHeight;
+			} else if (param.maxHeight && height > param.maxHeight) {
+				height = param.maxHeight;
+			}
+			var margin = param.margin || 0;
+			img = new Image(width + 2 * margin, height + 2 * margin);
+			img.setColor(java.awt.Color.WHITE);
+			img.fillRect(0, 0, img.width, img.height);
+			var g2d = img.getGraphics();
+			Packages.javax.swing.SwingUtilities.paintComponent(g2d, pane,
+					new java.awt.Container(), margin, margin, width, height);
+			g2d.dispose();
 		} catch (e) {
-			User.logError('HtmlRenderer#renderHtmlImage()', e);
+			User.logError('HtmlRenderer#render()', e);
 		}
 		return img; 
 	}
