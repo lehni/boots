d94bc842f01ce8416e3f970b1ffc0e1b5ce0b136
Jürg Lehni <juerg@scratchdisk.com>
2011-07-31 20:49:11 +0100

    Have code tags nested in other tags behave the same way as the ones appearing within text.

--- base/helma/Global/MarkupTags/CodeTag.js
+++ base/helma/Global/MarkupTags/CodeTag.js
@@ -5,11 +5,17 @@ CodeTag = MarkupTag.extend({
 	render: function(content, param, encoder, before, after) {
 		var code = content || this.arguments[0];
 		if (code) {
-			code = decode(code);
+			// TODO: Why was the code decoded? Seems to be wrong to do so
+			// code = decode(code);
 			// Nesting things in <code> seems to cause conflicts with Helma's
 			// encodeParagraphs that is somehow too smart about it, so use <pre>
 			// for blocks of code and <tt> for inlined code.
-			if ((typeof before != 'string' || /[\n\r]$|^$/.test(before))
+			if ((before != undefined || after != undefined 
+					// Handle nested tags differently, e.g. <b><code /></b>
+					// will not have before / after but should not be rendered
+					// as <pre>:
+					|| this.parent instanceof RootTag)
+					&& (typeof before != 'string' || /[\n\r]$|^$/.test(before))
 					&& (typeof after != 'string' || /^[\n\r]|^$/.test(after))) {
 				// Pass on all attributes to the pre tag.
 				var attributes = this.renderAttributes();
